8a097c3515e51a7286f24750159112963a3e9767
Elan Ruusamäe <glen@pld-linux.org>
2009-08-24 23:54:57 +0000

    - do some filtering to match tarball
    Changed files:
        branch.sh -> 1.2
        koffice-branch.diff -> 1.4

--- branch.sh
+++ branch.sh
@@ -5,7 +5,17 @@ prog=koffice
 ver=1.6.3
 branch=1.6
 
+filter() {
+	set -x
+	# - filter out already patched files (regenerated tarball???)
+	filterdiff -x krita/core/kis_perspective_math.cpp | \
+	filterdiff -x kspread/plugins/scripting/kspreadcore/krs_cell.cpp | \
+	filterdiff -x kspread/plugins/scripting/kspreadcore/krs_cell.h | \
+	# - remove rcs id
+	sed -e 's/$''Id:.*$/$''Id$/'
+}
+
 svn diff \
 	svn://anonsvn.kde.org/home/kde/tags/$prog/$ver/$prog \
 	svn://anonsvn.kde.org/home/kde/branches/$prog/$branch/$prog \
-	> koffice-branch.diff
+	| filter > $prog-branch.diff
